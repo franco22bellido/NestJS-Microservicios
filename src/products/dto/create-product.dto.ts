ad3ae4510407c8decd122d1d01014ea3afef7535
franco22bellido <franco.bellido22@gmail.com>
2024-11-10 21:44:15 -0300

    repaso: CRUD usando array de objetos

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,14 @@
-export class CreateProductDto {}
+import { IsNumber, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class CreateProductDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  description: string;
+
+  @IsNumber()
+  @Type(() => Number)
+  price: number;
+}
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,26 +1,40 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class ProductsService {
+  private products: Product[] = [];
+
   create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+    const { name, description, price } = createProductDto;
+
+    const newProduct = new Product(uuidv4(), name, description, price);
+    this.products.push(newProduct);
+    return newProduct;
   }
 
   findAll() {
-    return `This action returns all products`;
+    return this.products;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  findOne(id: string): Product {
+    const productFound = this.products.find((product) => product.id === id);
+    return productFound;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  update(id: string, updateProductDto: UpdateProductDto) {
+    this.products.map((product) => {
+      if (product.id === id) {
+        product = Object.assign(product, updateProductDto);
+        return;
+      }
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  remove(id: string) {
+    this.products = this.products.filter((product) => product.id !== id);
   }
 }
