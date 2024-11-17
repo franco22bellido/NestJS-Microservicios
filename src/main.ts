61f58cc9ee5dfe7448f75ecb1e37d1d7733d6237
franco22bellido <franco.bellido22@gmail.com>
2024-11-16 21:25:45 -0300

    fin de la sección 2: Reforzamiento de NestJS

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { envs } from './config/envs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,7 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(envs.port ?? 3000);
+  console.log(`Server running on port ${envs.port}`);
 }
 bootstrap();
